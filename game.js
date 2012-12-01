ba1236542969c8d867cf512207163ee7f011b776
Jay McGavren <jay@mcgavren.com>
2012-12-01 15:34:30 -0700

    Add enemy.

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprite'], function(GameCore, ResourceManager, keys, Sprite){
+  require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprite'], function(GameCore, ResourceManager, keys, Sprite){
 
   var x = 0;
   var y = 0;
@@ -6,11 +6,9 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
   var serenityy = 300;
   var serenityspeed = 2.5;
   var bulletSpeed = 4;
-  var enemyx = 0;
+  var enemyx = 300;
   var enemyy = 0;
-  var enemySpeed = 0;
-  var enemyAttackArray = 0;
-  var enemyAttack = [];
+  var enemySpeed = 4;
   var bulletArray = [];
 
 
@@ -19,20 +17,21 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
   var backImg = rm.loadImage('images/space2.jpg');
   var serenity = rm.loadImage('images/serenity.png');
   var bullet = rm.loadImage('images/bullet.png')
-  var enemy = rm.loadImage('images/bullet.png')
+  var enemy = rm.loadImage('images/enemy.png')
   
-  var game = new GameCore({
+ var game = new GameCore({
     canvasId: 'canvas',
     resourceManager: rm,
     draw: function(context){
       context.drawImage(backImg,x,y);
       context.drawImage(serenity, serenityx, serenityy);
+      context.drawImage(enemy, enemyx, enemyy);
       bulletArray.forEach (function(bulletObj){
         context.drawImage(bullet, bulletObj.x, bulletObj.y);
       });
-      // enemyArray.forEach (function(enemyObj){
-      //   context.drawImage(enemy, enemyObj.x, enemyObj.y);
-      // });
+     // enemyArray.forEach (function(enemyObj){
+       // context.drawImage(enemy, enemyObj.x, enemyObj.y);
+     // });
     },
     initInput: function(){
       //tells the input manager to listen for key events
@@ -44,16 +43,16 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
     },
     update: function(millis){
       //just an example showing how to check for presses, could be done more effeciently
-      if(this.inputManager.keyActions[keys.LEFT_ARROW].isPressed()){
+      if(this.inputManager.keyActions[keys.LEFT_ARROW].isPressed() && serenityx > 0){
         serenityx-= serenityspeed; 
       }
-      if(this.inputManager.keyActions[keys.RIGHT_ARROW].isPressed()){
+      if(this.inputManager.keyActions[keys.RIGHT_ARROW].isPressed() && serenityx < (this.width-serenity.width)){
         serenityx+= serenityspeed;
       }
-      if(this.inputManager.keyActions[keys.UP_ARROW].isPressed()){
+      if(this.inputManager.keyActions[keys.UP_ARROW].isPressed() && serenityy > 0){
         serenityy-=serenityspeed;
       }
-      if(this.inputManager.keyActions[keys.DOWN_ARROW].isPressed()){
+      if(this.inputManager.keyActions[keys.DOWN_ARROW].isPressed() && serenityy < (this.height-serenity.height)){
         serenityy+=serenityspeed;
       }
       if(this.inputManager.keyActions[keys.SPACE].getAmount()){
@@ -66,6 +65,7 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
       bulletArray.forEach (function(bulletObj){
         bulletObj.y = bulletObj.y - bulletSpeed;
       });
+      
       bulletArray = bulletArray.filter (function(bulletObj){
         if (bulletObj.y < 0){
           return false;
@@ -74,10 +74,13 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
           return true;
         }
       });
-      var enemyObj = new Sprite({x:enemyx,y:enemyy,w:enemy.width, h:enemy.height, dx:Math.random(enemySpeed), dy:Math.random(enemySpeed)});
-      // enemyArray.forEach (function(enemyArray){
-      //   enemyObj.y = Math.random(enemyObj.y) + enemySpeed;
-      // });
+   // var enemyObj = new Sprite({x:enemyx,y:enemyy,w:enemy.width, h:enemy.height, dx:0, dy:enemySpeed});
+  //  enemyArray.forEach (function(enemyObj){
+  //     enemyObj.y = enemyObj.y + enemySpeed;
+  //    });
+  //  if (bulletObj.y === enemyObj.y){
+  //    ()
+//    }
    }
   });
 

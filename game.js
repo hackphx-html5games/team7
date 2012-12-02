b034e3eb0012f06e52217e12356e5cdbb4376562
Jay McGavren <jay@mcgavren.com>
2012-12-01 17:26:51 -0700

    Rate limiting on enemy attacks, shots.

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+// Code: Alisa
+// Assets: Regi
+// Sound code, git wrangling: Jay
+
 require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprite'], function(GameCore, ResourceManager, keys, Sprite){
 
   var x = 0;
@@ -8,16 +12,36 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
   var bulletSpeed = 4;
   var enemyx = 300;
   var enemyy = 0;
-  var enemySpeed = 4;
+  var attackSpeed = 4;
+  var attackx = 0;
+  var attacky = 0;
   var bulletArray = [];
-
-
+  var attackArray = [];
+  
   //setup a ResourceManager to use in the game
-  var rm = new ResourceManager();
-  var backImg = rm.loadImage('images/space2.jpg');
-  var serenity = rm.loadImage('images/serenity.png');
-  var bullet = rm.loadImage('images/bullet.png')
-  var enemy = rm.loadImage('images/enemy.png')
+  var rm = new ResourceManager({
+    soundsDir: 'sound/',
+    imageDir: 'images/'
+  });
+
+  //Images.
+  var backImg = rm.loadImage('space2.jpg');
+  var serenity = rm.loadImage('serenity.png');
+  var bullet = rm.loadImage('bullet.png');
+  var enemy = rm.loadImage('enemy.png');
+  var attack = rm.loadImage('enemyAttack.png');
+  
+  //Sounds.
+  var music = rm.loadSound('music.mp3');
+  var musicSource = null;
+  var pew = rm.loadSound('pew.ogg');
+  
+  var gameStarted = false;
+  var gameOver = false;
+  
+  //Time accumulators.
+  var timeSinceAttack = 0;
+  var timeSinceShot = 0;
   
   var game = new GameCore({
     canvasId: 'canvas',
@@ -29,9 +53,9 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
       bulletArray.forEach (function(bulletObj){
         context.drawImage(bullet, bulletObj.x, bulletObj.y);
       });
-     // enemyArray.forEach (function(enemyObj){
-       // context.drawImage(enemy, enemyObj.x, enemyObj.y);
-     // });
+      attackArray.forEach (function(attackObj){
+        context.drawImage(attack, attackObj.x, attackObj.y);
+      });
     },
     initInput: function(){
       //tells the input manager to listen for key events
@@ -42,6 +66,62 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
       this.inputManager.addKeyAction(keys.SPACE, true);
     },
     update: function(millis){
+      
+      // --------------------------------------------------------
+      // Game status.
+      // --------------------------------------------------------
+      
+      if (!gameStarted) {
+        // musicSource = rm.playSound(music, true); //Loop music.
+        gameStarted = true;
+      }
+      if (gameOver) {
+        musicSource.noteOff();
+        gameOver = false;
+      }
+      
+      // --------------------------------------------------------
+      // Enemy attacks.
+      // --------------------------------------------------------
+      timeSinceAttack += millis;
+      if (timeSinceAttack > 100) {
+        if (attackArray.length < 30){
+          var attackObj = new Sprite({x:Math.random()*this.width,y:attacky,w:attack.width, h:attack.height, dx:0, dy:attackSpeed});
+          attackArray.push(attackObj);
+        }
+        timeSinceAttack = 0;
+      }
+      
+      bulletArray.forEach (function(bulletObj){
+        bulletObj.y = bulletObj.y - bulletSpeed;
+      });
+      
+      bulletArray = bulletArray.filter (function(bulletObj){
+        if (bulletObj.y < 0){
+          return false;
+        }
+        else{
+          return true;
+        }
+      });
+      
+      attackArray.forEach (function(attackObj){
+        attackObj.y = attackObj.y + attackSpeed;
+      });
+      
+      attackArray = attackArray.filter (function(attackObj){
+        if (attackObj.y > this.height){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }, this);
+      
+      // --------------------------------------------------------
+      // Input.
+      // --------------------------------------------------------
+      
       //just an example showing how to check for presses, could be done more effeciently
       if(this.inputManager.keyActions[keys.LEFT_ARROW].isPressed() && serenityx > 0){
         serenityx-= serenityspeed; 
@@ -55,33 +135,31 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/Sprit
       if(this.inputManager.keyActions[keys.DOWN_ARROW].isPressed() && serenityy < (this.height-serenity.height)){
         serenityy+=serenityspeed;
       }
-      if(this.inputManager.keyActions[keys.SPACE].getAmount()){
-        bulletx = serenityx+(serenity.width / 2);
-        bullety = serenityy+(serenity.height / 2);
-        var bulletObj = new Sprite({x:bulletx,y:bullety, w:bullet.width, h: bullet.height, dx:0, dy: -bulletSpeed});
-        bulletArray.push(bulletObj);
-      }
-
-      bulletArray.forEach (function(bulletObj){
-        bulletObj.y = bulletObj.y - bulletSpeed;
-      });
       
-      bulletArray = bulletArray.filter (function(bulletObj){
-        if (bulletObj.y < 0){
-          return false;
+      timeSinceShot += millis;
+      if(this.inputManager.keyActions[keys.SPACE].getAmount()){
+        if (timeSinceShot > 300) {
+          bulletx = serenityx+(serenity.width / 2);
+          bullety = serenityy+(serenity.height / 2);
+          var bulletObj = new Sprite({x:bulletx,y:bullety, w:bullet.width, h: bullet.height, dx:0, dy: -bulletSpeed});
+          bulletArray.push(bulletObj);
+          rm.playSound(pew);
+          timeSinceShot = 0;
         }
-        else{
-          return true;
-        }
-      });
-   // var enemyObj = new Sprite({x:enemyx,y:enemyy,w:enemy.width, h:enemy.height, dx:0, dy:enemySpeed});
-  //  enemyArray.forEach (function(enemyObj){
-  //     enemyObj.y = enemyObj.y + enemySpeed;
-  //    });
-  //  if (bulletObj.y === enemyObj.y){
-  //    ()
-//    }
-   }
+      }
+      
+      // --------------------------------------------------------
+      // Collision.
+      // --------------------------------------------------------
+      
+      var intersectSprite = function (bulletObj, attackObj) {
+        var distance_squared = Math.pow((bulletObj.x + (bulletObj.width/2)) - (attackObj.x + (attackObj.width/2)),2) + Math.pow((bulletObj.y + (bulletObj.height/2)) - (attackObj.y + (attackObj.height/2)),2);
+        var radii_squared = Math.pow(bulletObj.collisionRadius + attackObj.collisionRadius,2);
+        return distance_squared < radii_squared;  // true if intersect  
+      };
+
+    }
+
   });
 
   //if you want to take a look at the game object in dev tools
